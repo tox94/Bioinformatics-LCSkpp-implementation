38d2a2d5d70b31f66ce64f5cbd9013c53ed84db5
fvarga94 <fran.varga94@gmail.com>
2018-01-16 21:08:34 +0100

    relocation of sequence sampler

---
+++ tests/randomize.py
@@ -0,0 +1,14 @@
+import random
+import sys
+
+alfa=["A","C","G","T"]
+s1=s2=""
+
+for i in range(int(sys.argv[1])):
+    s1+=alfa[random.randrange(4)]
+    s2+=alfa[random.randrange(4)]
+
+file=open("fs"+sys.argv[1]+".txt","w")
+file.write(s1+"\n")
+file.write(s2+"\n")
+file.close()
