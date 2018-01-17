094c6e2b6e56f45b7ab1e2e6709f17771a6551ce
Toni <toni.bakarcic@fer.hr>
2018-01-17 13:59:54 +0100

    Algoritam znacajno ubrzan

--- java/MatchPairs.java
+++ java/MatchPairs.java
@@ -3,10 +3,10 @@ import java.util.Collections;
 import java.util.HashMap;
 
 public class MatchPairs {
-	
+
 	private int size;
 	private ArrayList<Pair> allPairs;
-	
+
 	public MatchPairs(int k, String x, String y) {
 		this.size = 0;
 		HashMap<Character, Integer> map = init(x, y);
@@ -14,10 +14,10 @@ public class MatchPairs {
 		ArrayList<Pair> pairs = new ArrayList<Pair>();
 		ArrayList<Pair> pairsE = new ArrayList<Pair>();
 		int mask = (int) (Math.pow(2, this.size*k)-1);
-		
-		int h = RHash(0, map.get(x.toCharArray()[0]), this.size, k, mask);
+
+		int h = RHash(0, map.get(x.charAt(0)), this.size, k, mask);
 		for(int i = 1; i< k; i++) {
-			h = RHash(h, map.get(x.toCharArray()[i]), this.size, k, mask);
+			h = RHash(h, map.get(x.charAt(i)), this.size, k, mask);
 		}
 		if (!table.containsKey(h)) {
 			ArrayList<Integer> temp = new ArrayList<Integer>();
@@ -25,7 +25,7 @@ public class MatchPairs {
 			table.put(h, temp);
 		}
 		for (int i = k; i<x.length(); i++) {
-			h = RHash(h, map.get(x.toCharArray()[i]), this.size, k, mask);
+			h = RHash(h, map.get(x.charAt(i)), this.size, k, mask);
 			if (!table.containsKey(h)) {
 				ArrayList<Integer> temp = new ArrayList<Integer>();
 				temp.add(i-k+1);
@@ -34,20 +34,20 @@ public class MatchPairs {
 				table.get(h).add(i-k+1);
 			}
 		}
-		
-		h = RHash(0, map.get(y.toCharArray()[0]), this.size, k, mask);
+
+		h = RHash(0, map.get(y.charAt(0)), this.size, k, mask);
 		for(int i = 1; i< k; i++) {
-			h = RHash(h, map.get(y.toCharArray()[i]), this.size, k, mask);
+			h = RHash(h, map.get(y.charAt(i)), this.size, k, mask);
 		}
 		if (table.containsKey(h)) {
 			ArrayList<Integer> temp = table.get(h);
 			for (int i=0; i< temp.size(); i++) {
 				pairs.add(new Pair(temp.get(i), 0, true));
 			}
-			
+
 		}
 		for (int j = k; j<y.length(); j++) {
-			h = RHash(h, map.get(y.toCharArray()[j]), this.size, k, mask);
+			h = RHash(h, map.get(y.charAt(j)), this.size, k, mask);
 			if (!table.containsKey(h)) {
 				continue;
 			}
@@ -57,10 +57,10 @@ public class MatchPairs {
 				pairs.add(new Pair(temp.get(i), val, true));
 			}
 		}
-		
+
 		int r = pairs.size();
 		pairs.forEach((p) -> {
-			pairsE.add(new Pair(p.getI() + k, p.getJ() + k, false));			
+			pairsE.add(new Pair(p.getI() + k, p.getJ() + k, false));
 		});
 		pairs.addAll(pairsE);
 		Collections.sort(pairs);
@@ -92,18 +92,18 @@ public class MatchPairs {
 		this.size = b;
 		return map;
 	}
-	
+
 	public int RHash (int h, int y, int b, int k, int mask) {
 		h <<= b;
 		h += y;
 		h &= mask;
 		return h;
 	}
-	
+
 	public ArrayList<Pair> getPairs(){
 		return this.allPairs;
 	}
-	
+
 	public int getSize() {
 		return this.size;
 	}
