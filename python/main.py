d1ac14fcd903b17a5435082572dd8af344b594df
fvarga94 <fran.varga94@gmail.com>
2018-01-17 18:49:29 +0100

    tests

--- python/main.py
+++ python/main.py
@@ -5,12 +5,12 @@ def main():
     if len(sys.argv)<3:
         print ("Please define k and input file")
         return
-    k=int(sys.argv[1])
-    file=open(sys.argv[2])
+    k=int(sys.argv[2])
+    file=open(sys.argv[1])
     string_1=file.readline().strip().lower()
     string_2=file.readline().strip().lower()
     file.close()
     LCSkpp(k,string_1,string_2)
-    
+
 if __name__ == '__main__':
     main()
--- tests/timer.py
+++ tests/timer.py
@@ -5,20 +5,20 @@ FNULL = open(os.devnull, 'w')
 files=check_output(["ls","test_sequences"])
 files_list=files.decode().split("\n")[0:-1]
 p=["python","c","java","c++"]
-pro=p[int(sys.argv[1])]
 
-for k in [15,20,25]:
-    for f in files_list:
-        print (pro,k,f)
-        if pro=="python":
-            call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f, "python3", "../python/main.py", str(k), "test_sequences/"+f]
-            call(call_l)
-        elif pro=="java":
-            call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f, "java", "-cp", "../java/", "Main", "main", str(k), "test_sequences/"+f]
-            call(call_l)
-        elif pro=="c":
-            call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f, "../c/main", "test_sequences/"+f,str(k)]
-            call(call_l)
-        elif pro=="c++":
-            call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f,"../cpp/main","test_sequences/"+f,str(k)]
-            call(call_l)
+for pro in p:
+    for k in [15,20,25]:
+        for f in files_list:
+            print (pro,k,f)
+            if pro=="python":
+                call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f, "python3", "../python/main.py", "test_sequences/"+f, str(k)]
+                call(call_l)
+            elif pro=="java":
+                call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f, "java", "-cp", "../java/", "Main", "main", "test_sequences/"+f, str(k)]
+                call(call_l)
+            elif pro=="c":
+                call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f, "../c/main", "test_sequences/"+f, str(k)]
+                call(call_l)
+            elif pro=="c++":
+                call_l=["/usr/bin/time","-v","-o","tested/"+pro+"_tested_k_"+str(k)+f,"../cpp/main","test_sequences/"+f, str(k)]
+                call(call_l)
